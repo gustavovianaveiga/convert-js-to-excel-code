07c9479c7a542d698ffe86952b3fefd50549d794
gustavovianaveiga <gustavovianaveiga721@gmail.com>
2022-11-04 22:22:01 -0300

    first commit

---
+++ code.js
@@ -0,0 +1,31 @@
+function Download() {
+    const wb = XLSX.utils.book_new();
+
+    wb.Props = {
+        Title: 'titulo',
+        Subject: 'descricao',
+        Author: 'autor',
+        CreatedDate: new Date(),
+    };
+
+    wb.SheetNames.push('tabela 1');
+
+    const dados = [
+        ['Nome', 'Usuario', 'E-mail', ' preço'],
+        ['gustavo', 'login123', 'gustavovianaveiga721@gmail.com', 'R$8'],
+        ['luis', 'log333', 'gustavovianaveiga12@gmail.com', 'R$5'],
+        ['vagner', 'lsada22', 'gustavovianaveiga34@gmail.com', 'R$4']
+    ];
+
+    const ws = XLSX.utils.aoa_to_sheet(dados);
+
+    wb.Sheets['tabela 1'] = ws;
+
+    XLSX.writeFile(wb, 'nome do arquivo.xlsx', { bookType: 'xlsx', type: 'bynary' });
+
+}
+
+const button = document.querySelector('#button');
+button.addEventListener('click', () => {
+    Download()
+});
